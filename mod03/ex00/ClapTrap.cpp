dde94e4197cdbd158401c5d9f285a3c13d8272de
Yangson Choi <yachoi@c5r7s4.42seoul.kr>
2022-12-22 18:49:19 +0900

    Add mod03_ex00 made claptrap class

---
+++ mod03/ex00/ClapTrap.cpp
@@ -0,0 +1,66 @@
+// Copyright 2022 Yangson Choi. All rights reserved.
+
+#include "ex00/ClapTrap.hpp"
+
+#include <iostream>
+
+
+using std::cout;
+using std::endl;
+using std::string;
+
+ClapTrap::ClapTrap()
+    : name_(kName), current_hp_(kFullHP),
+    current_ep_(kFullEP), attack_damage_(kDefaultAD) {
+  cout << get_name() << ": Recompiling my combat code(Default constructor)" << endl;
+}
+
+ClapTrap::ClapTrap(const string& name)
+    : name_(name), current_hp_(kFullHP),
+    current_ep_(kFullEP), attack_damage_(kDefaultAD) {
+  cout << get_name() << ": go -- Oh that's me(Name constructor)" << endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap& c)
+    : name_(c.get_name()), current_hp_(c.get_hp()), current_ep_(c.get_ep()),
+      attack_damage_(c.get_damage()) {
+  cout << get_name() << ": Viva la Robolution!(Copy constructor)" << endl;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& c) {
+  cout << "Copy assignment operator called" << endl;
+  name_ = c.get_name();
+  current_hp_ = c.get_hp();
+  current_ep_ = c.get_ep();
+  attack_damage_ = c.get_damage();
+  return *this;
+}
+
+ClapTrap::~ClapTrap() {
+  cout << "I see three flashing red lights...(destructor)" << endl;
+}
+
+void ClapTrap::attack(const std::string& target) {
+  cout << get_name() << ": Are you bulletproof or...? Whoops! Guess not!" << endl;
+  cout << "System: ClapTrap " << get_name() << " attacks " << target
+      << ", causing " << get_damage() << " points of damage!" << endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount) {
+  cout << get_name() << ": I'm leaking!" << endl;
+  cout << "System: ClapTrap " << get_name() << " take " << amount
+      << " damage. ";
+  if (get_hp() <= amount)
+    set_hp(0);
+  else
+    set_hp(get_hp() - amount);
+  cout << "[ " << get_hp() << " ]" << endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount) {
+  cout << get_name() << ": Sweet life juice!" << endl;
+  cout << "System: ClapTrap " << get_name() << " heal " << amount
+      << " hit points. ";
+    set_hp(get_hp() + amount);
+  cout << "[ " << get_hp() << " ]" << endl;
+}
---
+++ mod03/ex00/ClapTrap.hpp
@@ -0,0 +1,46 @@
+// Copyright 2022 Yangson Choi. All rights reserved.
+
+#ifndef MOD03_EX00_CLAPTRAP_HPP_
+#define MOD03_EX00_CLAPTRAP_HPP_
+
+#include <string>
+
+#define DEFAULT_NAME "CL4P-TP"
+
+class ClapTrap {
+ public :
+  ClapTrap();
+  ClapTrap(const std::string&);
+  ClapTrap(const ClapTrap&);
+
+  ClapTrap& operator=(const ClapTrap&);
+
+  ~ClapTrap();
+
+  void set_name(const std::string name)  { name_ = name; } 
+  void set_hp(const unsigned int hp)  { current_hp_ = hp; }
+  void set_ep(const unsigned int ep)  { current_ep_ = ep; }
+  void set_damage(const unsigned int damage)  { attack_damage_ = damage; }
+  
+  std::string	get_name() const { return name_; }
+  int get_hp() const { return current_hp_; }
+  int get_ep() const { return current_ep_; }
+	int get_damage() const { return attack_damage_; }
+
+  void attack(const std::string&);
+  void takeDamage(unsigned int);
+  void beRepaired(unsigned int);
+
+ private :
+  const char* kName = "CL4P-TP";
+  static const unsigned int kFullHP = 10;
+  static const unsigned int kFullEP = 10;
+  static const unsigned int kDefaultAD = 0;
+
+  std::string name_;
+  unsigned int current_hp_;
+  unsigned int current_ep_;
+  unsigned int attack_damage_;
+};
+
+#endif  // MOD03_EX00_CLAPTRAP_HPP_
